81795124fb184eba51bda5c89942eaf400b3f730
Anthony Meade <antnymeade@gmail.com>
2023-11-10 20:23:31 +0000

    Update function for Influx.js

--- function for Influx.js
+++ function for Influx.js
@@ -1,4 +1,4 @@
-// script has multiple locations, this filters them into to messurements (Tables) 
+//script has multiple locations, this filters them into to messurements (Tables) 
 //for influx2 and deletes it from the payload as it useless row. 
 msg.measurement = msg.payload.Location;
 delete msg.payload.Location
