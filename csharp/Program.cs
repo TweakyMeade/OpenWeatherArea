648d9c45682066f2f6d1327b28ddfd87f39499f9
Anthony Meade <antnymeade@gmail.com>
2023-12-09 18:23:12 +0000

    csharp

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.Write("test");
+﻿using Microsoft.Extensions.Configuration;
+
+var keyRing = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+
+string apiKey = configuration.;
+int location = 1;
+
+
+using (HttpClient client = new HttpClient())
+{
+    string urlAPI = $"https://api.openweathermap.org/data/2.5/weather?appid={apiKey}&id={location}";
+
+    Console.WriteLine(urlAPI);
+}
