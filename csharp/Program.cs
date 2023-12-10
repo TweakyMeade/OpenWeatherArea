63a376d5d2d31ebcab7a90d741dc61c6ee429cc4
Anthony Meade <antnymeade@gmail.com>
2023-12-10 11:50:20 +0000

    added package

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
 
 var keyRing = new ConfigurationBuilder().AddJsonFile("appsettings.json");
 
@@ -9,6 +10,7 @@ int location = 1;
 using (HttpClient client = new HttpClient())
 {
     string urlAPI = $"https://api.openweathermap.org/data/2.5/weather?appid={apiKey}&id={location}";
+    var json = JToken.Parse(urlAPI);
 
     Console.WriteLine(urlAPI);
 }
